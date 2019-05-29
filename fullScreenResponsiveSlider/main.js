86559f8f4d37b985a047f1f63c7c9c75c406fe16
Poputa Alexandru <alexandersupertramp94@yahoo.com>
2019-05-29 15:09:15 +0300

    Slider

---
+++ fullScreenResponsiveSlider/main.js
@@ -0,0 +1,41 @@
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+  const current = document.querySelector(".current");
+  // Remove current class
+  current.classList.remove("current");
+  // Check for next slide
+  if (current.nextElementSibling) {
+    // Add current to next sibling
+    current.nextElementSibling.classList.add("current");
+  } else {
+    // Add current to start when we hit the last current class element
+    slides[0].classList.add("current");
+  }
+  setTimeout(() => current.classList.remove("current"));
+};
+
+const prevSlide = () => {
+  const current = document.querySelector(".current");
+  // Remove current class
+  current.classList.remove("current");
+  // Check for previous slide
+  if (current.previousElementSibling) {
+    // Add current to previous sibling
+    current.previousElementSibling.classList.add("current");
+  } else {
+    // Add current to last
+    slides[slides.length - 1].classList.add("current");
+  }
+  setTimeout(() => current.classList.remove("current"));
+};
+
+// Button events
+next.addEventListener("click", nextSlide);
+
+prev.addEventListener("click", prevSlide);
